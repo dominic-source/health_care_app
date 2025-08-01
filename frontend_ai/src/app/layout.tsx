7bdd24fd9bbcb160af422b6efc90c7e512fa2e74
dominic-source <chinonsodomnic@gmail.com>
2025-08-01 16:48:06 +0100

    fix: update app layout page

--- frontend_ai/src/app/layout.tsx
+++ frontend_ai/src/app/layout.tsx
@@ -1,20 +1,14 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import SidebarNavigation from '@/components/SidebarNavigation';
+import { geistMono, geistSans } from '@/constant';
+import { ThemeProvider } from '@/context/ThemeProvider';
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Electronic Medical Records",
-  description: "Created by the Kadabite company",
+  title: 'Electronic Medical Records',
+  description: 'Created by the Kadabite company',
+  viewport: 'width=device-width, initial-scale=1',
+  robots: 'index, follow',
 };
 
 export default function RootLayout({
@@ -23,11 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className={`${geistSans.className} ${geistMono.className}`}>
+      <body className="antialiased">
+        <ThemeProvider>
+          <SidebarNavigation>{children}</SidebarNavigation>
+        </ThemeProvider>
       </body>
     </html>
   );
